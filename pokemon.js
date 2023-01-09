d10df670dbe394c4279c62be7223c9b7ae292f88
HsN <hsn@HsNs-MacBook-Pro.local>
2023-01-09 19:08:03 +0500

    updated total pokemons loeaded

--- pokemon.js
+++ pokemon.js
@@ -3,7 +3,7 @@
 const container = document.querySelector('#container');
 const baseURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
 
-for (let i = 1; i <= 151; i++) {
+for (let i = 1; i <= 5000; i++) {
     const pokemon = document.createElement('div');
     pokemon.classList.add('pokemon');
     const label = document.createElement('span');
